57101e654690a4f778322dbfc343a2f7f313367e
Samg2000 <smlgarg123@gmail.com>
2021-01-16 00:24:28 +0530

    Some Refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
+// import 'package:googleapis/bigtableadmin/v2.dart';
 import 'package:newmoneytracker/Data/Data.dart';
+import 'package:newmoneytracker/Screens/backup_screen.dart';
 import 'package:newmoneytracker/Screens/history.dart';
 import 'package:provider/provider.dart';
+import './Data/User.dart';
 import 'Data/constants.dart';
 import 'Screens/main_screen.dart';
 
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (BuildContext context)=>Data(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (BuildContext context) => Data(),
+        ),
+        ChangeNotifierProvider(
+          create: (BuildContext context) => UserData(),
+        ),
+      ],
       child: MaterialApp(
         theme: ThemeData.dark().copyWith(
           accentColor: accentColor,
@@ -29,8 +41,9 @@ class MyApp extends StatelessWidget {
           ),
         ),
         routes: {
-          History.route:(context)=>History(),
-          HomeScreen.route:(context)=>HomeScreen()
+          History.route: (context) => History(),
+          HomeScreen.route: (context) => HomeScreen(),
+          BackupScreen.route:(context)=>BackupScreen(),
         },
         initialRoute: HomeScreen.route,
       ),
